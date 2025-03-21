74af04a552bba9a623025afa0c5027c9f5605590
Unicatm <alexandra.tertiscu@gmail.com>
2025-03-21 20:03:32 +0200

    update sidebar

--- client/src/components/sideNav/MiddleNav.jsx
+++ client/src/components/sideNav/MiddleNav.jsx
@@ -1,22 +1,34 @@
 import { Link, useLocation } from "react-router-dom";
 import navLinks from "./navLinks";
 
-function MiddleNav() {
+function MiddleNav({ isExpanded }) {
   const location = useLocation();
 
   return (
-    <div className="flex pl-4 pt-8 flex-col gap-2">
+    <div
+      className={`flex p-3 pt-8 flex-col gap-2 items-center${
+        isExpanded ? "w-max" : "w-fit"
+      }`}
+    >
       {navLinks.map((link) => {
         const isActive = location.pathname.startsWith(link.route);
 
         return (
-          <Link to={link.route} key={link.route}>
+          <Link
+            to={link.route}
+            key={link.route}
+            className={`transition-all duration-300 ease-in-out ${
+              isExpanded ? "w-52" : "w-fit"
+            }`}
+          >
             <div
-              className={`group relative flex p-2 gap-4 transition-all duration-300 ease-in-out hover:cursor-pointer before:absolute before:bg-blue-700 before:rounded-2xl before:inset-y-0 before:right-0 before:block before:w-1 before:transition-colors before:duration-300 before:ease-in-out ${
+              className={`group relative flex p-2 gap-4 rounded-sm hover:bg-linear-to-r hover:from-blue-50 hover:to-blue-100/80 hover:cursor-pointer hover:transition-colors hover:duration-300 hover:ease-in-out ${
                 isActive
-                  ? "text-blue-700 before:opacity-100"
-                  : "text-gray-400 before:opacity-0 hover:before:opacity-100"
-              }`}
+                  ? "text-blue-700 bg-linear-to-r from-blue-50 to-blue-100/80"
+                  : "text-gray-400"
+              }
+              ${isExpanded ? "" : "w-fit"}
+             `}
             >
               <link.icon
                 className={`h-6 w-6 transition-all duration-300 ease-in-out ${
@@ -25,8 +37,9 @@ function MiddleNav() {
               />
               <p
                 className={`font-medium transition-all duration-300 ease-in-out ${
-                  isActive ? "text-blue-950" : "group-hover:text-blue-950"
-                }`}
+                  isActive ? "text-blue-800" : "group-hover:text-blue-950"
+                }
+                ${isExpanded ? "opacity-100 block" : "opacity-0 hidden"}`}
               >
                 {link.title}
               </p>
--- client/src/components/sideNav/SideNav.jsx
+++ client/src/components/sideNav/SideNav.jsx
@@ -1,12 +1,46 @@
+import { useEffect, useState } from "react";
 import MiddleNav from "./MiddleNav";
+import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/16/solid";
+
+import logo from "../../assets/logo.png";
 
 function SideNav() {
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  useEffect(() => {
+    console.log(isExpanded);
+  }, [isExpanded]);
+
   return (
-    <div className="flex flex-col min-w-fit h-full w-1/6 divide-y-1 divide-blue-950/20">
-      <h1 className="text-2xl text-left font-black text-blue-950 pl-6 pb-8 pt-8">
-        Dan Pescarul
-      </h1>
-      <MiddleNav />
+    <div
+      className={`flex flex-col min-h-screen bg-white shadow-lg transition-all duration-300 ease-in-out ${
+        isExpanded ? "w-60" : "w-16"
+      }`}
+    >
+      <div
+        className={`flex flex-row justify-between px-4 py-4 items-center border-b-[1px] border-blue-800/20 ${
+          isExpanded ? "gap-2" : ""
+        }`}
+      >
+        <div className="flex items-center gap-2 overflow-hidden max-w-full">
+          <img src={logo} alt="Logo" className="w-12 h-11 min-w-12 min-h-11" />
+          <h1
+            className={`text-2xl whitespace-nowrap font-black text-blue-950 transition-all duration-300 ${
+              isExpanded ? "opacity-100 w-auto" : "opacity-0 w-0 hidden"
+            }`}
+          >
+            Dan P.
+          </h1>
+        </div>
+        <button
+          onClick={() => setIsExpanded((prev) => !prev)}
+          className="cursor-pointer min-w-8 min-h-8 w-8 h-8 text-blue-950 rounded-sm p-1 hover:bg-blue-50 transition-all duration-300"
+        >
+          {isExpanded ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+        </button>
+      </div>
+
+      <MiddleNav isExpanded={isExpanded} />
     </div>
   );
 }
